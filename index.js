16bc96e3c02ac2f784b4426775bfa524bd275b1e
anisa6121 <you@example.com>
2022-10-26 11:30:52 +0600

    course-item-complete

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+const Port = process.env.port || 5000;
+
+const courseId = require("./data/course.json")
+
+
+    
+app.get("/", (req, res) => {
+	res.send("Server Is Running");
+});
+
+
+app.get("/course", (req, res) => {
+	res.send(courseId);
+}); 
+
+
+
+
+app.listen(Port, () => {
+	console.log("Server is Running On Wesite", Port);
+});
